278c38ddade1c1dfa4fdc60603547879c8fe6fdc
Reza Abedi <reza.abd95@yahoo.com>
2024-04-23 17:34:12 +0330

    Added project and first configs

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Autocomplete from "./Autocomplete";
+
+function App() {
+  return (
+    <>
+      <Autocomplete />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Autocomplete.jsx
@@ -0,0 +1,5 @@
+function Autocomplete() {
+  return <div></div>;
+}
+
+export default Autocomplete;
